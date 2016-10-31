1e667213c9faf0716f2d9b7286d70f7bd857f77c
ItsDavidThai <itsdavidthai@gmail.com>
2016-10-31 15:51:59 -0700

    add express server config file

---
+++ server/server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var morgan = require('morgan')
+var bodyParser = require('body-parser');
+
+// start express
+var app = express();
+// log middleware
+app.use(morgan('dev'));
+
+// use bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// serve static files from root
+app.use(express.static(path.resolve(__dirname, '../')));
+
+// redirect all requests to react-router
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {
+    root: path.resolve(__dirname, '../src')
+  });
+});
+
+// log errors
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// set port
+var port = process.env.PORT || 1337;
+
+// listen on port
+app.listen(port);
+
+
+console.log("Server is listening on port " + port);
+
+module.exports = app;
