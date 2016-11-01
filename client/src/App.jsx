dc24ab129ba581789bd0ed6d403e8392d7cc74a8
ItsDavidThai <itsdavidthai@gmail.com>
2016-10-31 18:21:00 -0700

    Add filter by sector

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loanData:[]
+      loanData:[],
+      sector: 'All'
     }
   }
 
@@ -16,18 +17,44 @@ class App extends Component {
     }.bind(this))
   }
 
+  handleFundOptionChange(event) {
+    this.setState({sector:event.target.value})
+  }
+
   render() {
     let info = <div></div>;
 
     if (this.state.loanData.data){
+      let filteredBySector = this.state.loanData.data.loans.filter((loan) => loan.sector === this.state.sector)
       console.log(this.state.loanData)
-      info = this.state.loanData.data.loans.map((loan) => <LoanItem data={loan} />, this)
+      if(this.state.sector === 'All'){
+        info = this.state.loanData.data.loans.map((loan) => <LoanItem data={loan} />, this)
+      } else {
+        info = filteredBySector.map((loan) => <LoanItem data={loan} />, this)
+      }
     }
 
     return (
       <div>
           <h2>Coding Challenge</h2>
       <div>
+      <select value={this.state.sector} onChange={this.handleFundOptionChange.bind(this)}>
+
+        <option value="All">All</option>
+        <option value="Agriculture">Agriculture</option>
+        <option value="Arts">Arts</option>
+        <option value="Clothing">Clothing</option>
+        <option value="Construction">Construction</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Food">Food</option>
+        <option value="Health">Health</option>
+        <option value="Housing">Housing</option>
+        <option value="Manufacturing">Manufacturing</option>
+        <option value="Personal use">Personal use</option>
+        <option value="Retail">Retail</option>
+        <option value="Services">Services</option>
+        <option value="Wholesale">Wholesale</option>
+      </select>
 
       </div>
           {info}
