9a8e3a5725f459f59ac1a56575a365332785adb3
ItsDavidThai <itsdavidthai@gmail.com>
2016-10-31 17:49:37 -0700

    adjust webpack config to new wrraped src files

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 
-var APP_DIR = path.resolve(__dirname, './src');
-var BUILD_DIR = path.resolve(__dirname, './dist');
+var APP_DIR = path.resolve(__dirname, './client/src');
+var BUILD_DIR = path.resolve(__dirname, './client/dist');
 
 var config = {
   devtool: 'inline-source-map',
@@ -18,7 +18,7 @@ var config = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
+        loader: 'babel',
         exclude: /node_modules/,
         query: {
             presets: ['react', 'es2015']
