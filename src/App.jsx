65648427293eacf40dfd10e7c23fe5864721cafb
ItsDavidThai <itsdavidthai@gmail.com>
2016-10-31 17:17:25 -0700

    add app component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  render() {
+
+    return (
+      <div>
+          <h2>Hello World</h2>
+      </div>
+
+    );
+  }
+}
+
+export default App;
